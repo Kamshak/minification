588bdb54c13766359fa1122a33b7a5dc5007e8ab
Valentin Funk <funk.valentin@gmail.com>
2016-05-19 08:52:39 +0200

    Another one

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,18 +1,33 @@
-import angular from 'angular';
+import 'angular';
 import 'angular-meteor';
+import 'angular-ui-router';
 
 var app = angular.module('ps2StoreApp', [
   'angular-meteor',
+  'ui.router'
 ]);
 
-app.config(function($locationProvider) {
+app.config(function($locationProvider, $stateProvider) {
   $locationProvider.html5Mode(true)
+
+  $stateProvider
+  .state('test', {
+    url: '/test',
+    template: '<p>{{test}}</p>',
+    controller: ($scope) => { $scope.test = 123; }
+  });
 });
 
 onReady = function() {
   angular.bootstrap(document, ['ps2StoreApp']);
 };
 
+app.run(function($rootScope, $state, $timeout, $transitions) {
+  $state.
+  $transitions.onError({to: () => true }, ($error$, $state) => {
+    console.log($error$);
+  });
+});
 
 if(Meteor.isCordova) {
   angular.element(document).on('deviceready', onReady);
--- client/routes-base.js
+++ client/routes-base.js
@@ -1,6 +1,3 @@
-'use strict';
-
-import angular from 'angular';
 angular.module('ps2StoreApp')
 .config(function($httpProvider) {
   $httpProvider.useLegacyPromiseExtensions(true);
